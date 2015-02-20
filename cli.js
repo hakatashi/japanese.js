b5bec5bbfcc9c22a90583e58510fe1bae6846c69
Koki Takahashi <hakatasiloving@gmail.com>
2015-02-21 01:15:10 +0900

    noexpandtab

--- cli.js
+++ cli.js
@@ -4,23 +4,23 @@ var meow = require('meow');
 var japanese = require('./');
 
 var cli = meow({
-    help: [
-        'Usage:',
-        '  japanese <input> [options]',
-        '',
-        'Options:',
-        '  -H, --hiraganize   Hiraganize input string',
-        '  -K, --katakanize   katakanize input string',
-        '',
-        'Example',
-        '  japanese ヱヴァンゲリヲン --hiraganize'
-    ].join('\n')
+	help: [
+		'Usage:',
+		'  japanese <input> [options]',
+		'',
+		'Options:',
+		'  -H, --hiraganize   Hiraganize input string',
+		'  -K, --katakanize   katakanize input string',
+		'',
+		'Example',
+		'  japanese ヱヴァンゲリヲン --hiraganize'
+	].join('\n')
 });
 
 if (cli.flags.H || cli.flags.hiraganize) {
-    console.log(japanese.hiraganize(cli.input[0]));
+	console.log(japanese.hiraganize(cli.input[0]));
 } else if (cli.flags.K || cli.flags.katakanize) {
-    console.log(japanese.katakanize(cli.input[0]));
+	console.log(japanese.katakanize(cli.input[0]));
 } else {
-    console.log(cli.input[0]);
+	console.log(cli.input[0]);
 }
