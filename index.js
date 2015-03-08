c7ae6e4d52f034c156a8532203b1c85586bb1448
Koki Takahashi <hakatasiloving@gmail.com>
2015-03-08 17:45:53 +0900

    Add romanization table

--- index.js
+++ index.js
@@ -40,8 +40,166 @@ japanese.specialKatakanizationTable = {
 };
 
 japanese.romanizationTable = {
-
-}
+	'あ': 'a',
+	'い': 'i',
+	'う': 'u',
+	'え': 'e',
+	'お': 'o',
+	'か': 'ka',
+	'き': 'ki',
+	'く': 'ku',
+	'け': 'ke',
+	'こ': 'ko',
+	'さ': 'sa',
+	'し': 'si',
+	'す': 'su',
+	'せ': 'se',
+	'そ': 'so',
+	'た': 'ta',
+	'ち': 'ti',
+	'つ': 'tu',
+	'て': 'te',
+	'と': 'to',
+	'な': 'na',
+	'に': 'ni',
+	'ぬ': 'nu',
+	'ね': 'ne',
+	'の': 'no',
+	'は': 'ha',
+	'ひ': 'hi',
+	'ふ': 'hu',
+	'へ': 'he',
+	'ほ': 'ho',
+	'ま': 'ma',
+	'み': 'mi',
+	'む': 'mu',
+	'め': 'me',
+	'も': 'mo',
+	'や': 'ya',
+	'ゆ': 'yu',
+	'よ': 'yo',
+	'ら': 'ra',
+	'り': 'ri',
+	'る': 'ru',
+	'れ': 're',
+	'ろ': 'ro',
+	'わ': 'wa',
+	'ゐ': 'wi',
+	'ゑ': 'we',
+	'を': 'wo',
+	'ん': 'n',
+	'が': 'ga',
+	'ぎ': 'gi',
+	'ぐ': 'gu',
+	'げ': 'ge',
+	'ご': 'go',
+	'ざ': 'za',
+	'じ': 'zi',
+	'ず': 'zu',
+	'ぜ': 'ze',
+	'ぞ': 'zo',
+	'だ': 'da',
+	'ぢ': 'di',
+	'づ': 'du',
+	'で': 'de',
+	'ど': 'do',
+	'ば': 'ba',
+	'び': 'bi',
+	'ぶ': 'bu',
+	'べ': 'be',
+	'ぼ': 'bo',
+	'ゔ': 'vu',
+	'ぱ': 'pa',
+	'ぴ': 'pi',
+	'ぷ': 'pu',
+	'ぺ': 'pe',
+	'ぽ': 'po',
+	'きゃ': 'kya',
+	'きゅ': 'kyu',
+	'きぇ': 'kye',
+	'きょ': 'kyo',
+	'しゃ': 'sya',
+	'しゅ': 'syu',
+	'しぇ': 'sye',
+	'しょ': 'syo',
+	'ちゃ': 'tya',
+	'ちゅ': 'tyu',
+	'ちぇ': 'tye',
+	'ちょ': 'tyo',
+	'にゃ': 'nya',
+	'にゅ': 'nyu',
+	'にぇ': 'nye',
+	'にょ': 'nyo',
+	'ひゃ': 'hya',
+	'ひゅ': 'hyu',
+	'ひぇ': 'hye',
+	'ひょ': 'hyo',
+	'みゃ': 'mya',
+	'みゅ': 'my',
+	'みぇ': 'mye',
+	'みょ': 'myo',
+	'りゃ': 'rya',
+	'りゅ': 'ryu',
+	'りぇ': 'rye',
+	'りょ': 'ryo',
+	'ぎゃ': 'gya',
+	'ぎゅ': 'gyu',
+	'ぎぇ': 'gye',
+	'ぎょ': 'gyo',
+	'じゃ': 'zya',
+	'じゅ': 'zyu',
+	'じぇ': 'zye',
+	'じょ': 'zyo',
+	'ぢゃ': 'dya',
+	'ぢゅ': 'dyu',
+	'ぢぇ': 'dye',
+	'ぢょ': 'dyo',
+	'びゃ': 'bya',
+	'びゅ': 'byu',
+	'びぇ': 'bye',
+	'びょ': 'byo',
+	'ゔぁ': 'va',
+	'ゔぃ': 'vi',
+	'ゔぇ': 've',
+	'ゔぉ': 'vo',
+	'ぴゃ': 'pya',
+	'ぴゅ': 'pyu',
+	'ぴぇ': 'pye',
+	'ぴょ': 'pyo',
+	'てゃ': 'tea',
+	'てぃ': 'tei',
+	'てゅ': 'teu',
+	'てぇ': 'tee',
+	'てょ': 'teo',
+	'つぁ': 'tua',
+	'つぃ': 'tui',
+	'つぇ': 'tue',
+	'つぉ': 'tuo',
+	'づぁ': 'dua',
+	'づぃ': 'dui',
+	'づぇ': 'due',
+	'づぉ': 'duo',
+	'ふぁ': 'hua',
+	'ふぃ': 'hui',
+	'ふぇ': 'hue',
+	'ふぉ': 'huo',
+	'いぇ': 'ye',
+	'くゎ': 'kwa',
+	'ぐゎ': 'gwa',
+	'ー': '-',
+	'ぁ': 'a',
+	'ぃ': 'i',
+	'ぅ': 'u',
+	'ぇ': 'e',
+	'ぉ': 'o',
+	'ゃ': 'ya',
+	'ゅ': 'yu',
+	'ょ': 'yo',
+	'っ': 'tu',
+	'ゎ': 'wa',
+	'ヵ': 'ka',
+	'ヶ': 'ke',
+};
 
 japanese.defaultRomanizationConfig = {
 	'し': 'shi',
@@ -50,12 +208,15 @@ japanese.defaultRomanizationConfig = {
 	'ふ': 'fu',
 	'じ': 'ji',
 	'ぢ': 'ji',
+	'づ': 'zu',
 	'おう': 'ō',
 	'んあ': 'n\'a',
 	'んば': 'nba',
 	'っち': 'tchi',
+	'ゐ': 'i',
 	'を': 'o',
 	diacritical: true,
+	punctuation: true,
 };
 
 japanese.romanizationConfigs = {
@@ -78,6 +239,7 @@ japanese.romanizationConfigs = {
 		'ぢ': 'di',
 		'おう': 'ô',
 		'っち': 'tti',
+		'ゐ': 'wi',
 		'を': 'wo',
 	},
 };
@@ -116,7 +278,7 @@ japanese.romanize = function (string, config) {
 		config = japanese.romanizationConfigs[config];
 
 		if (typeof config === 'undefined') {
-			throw new ReferenceError('Romanization method ' + config + ' is undefined');
+			throw new ReferenceError('Romanization method "' + config + '" is undefined');
 		}
 	}
 
@@ -124,6 +286,101 @@ japanese.romanize = function (string, config) {
 		config = extend({}, japanese.defaultRomanizationConfig, config);
 	} else {
 		throw new Error('You specified unknown config to japanese.romanize');
+	}
+
+	var table = extend({}, japanese.romanizationTable);
+
+	if (config['し'] === 'shi') {
+		extend(table, {
+			'し': 'shi',
+			'しゃ': 'sha',
+			'しゅ': 'shu',
+			'しぇ': 'she',
+			'しょ': 'sho',
+		});
+	}
+
+	if (config['ち'] === 'chi') {
+		extend(table, {
+			'ち': 'chi',
+			'ちゃ': 'cha',
+			'ちゅ': 'chu',
+			'ちぇ': 'che',
+			'ちょ': 'cho',
+		});
+	}
+
+	if (config['つ'] === 'tsu') {
+		extend(table, {
+			'つ': 'tsu',
+			'つぁ': 'tsa',
+			'つぃ': 'tsi',
+			'つぇ': 'tse',
+			'つぉ': 'tso',
+		});
+	}
+
+	if (config['ふ'] === 'fu') {
+		extend(table, {
+			'ふ': 'fu',
+			'ふぁ': 'fa',
+			'ふぃ': 'fi',
+			'ふぇ': 'fe',
+			'ふぉ': 'fo',
+		});
+	}
+
+	if (config['じ'] === 'ji') {
+		extend(table, {
+			'じ': 'ji',
+			'じゃ': 'ja',
+			'じゅ': 'ju',
+			'じぇ': 'je',
+			'じょ': 'jo',
+		});
+	}
+
+	if (config['ぢ'] === 'ji') {
+		extend(table, {
+			'ぢ': 'ji',
+			'ぢゃ': 'ja',
+			'ぢゅ': 'ju',
+			'ぢぇ': 'je',
+			'ぢょ': 'jo',
+		});
+	}
+
+	if (config['ぢ'] === 'zi') {
+		extend(table, {
+			'ぢ': 'zi',
+			'ぢゃ': 'zia',
+			'ぢゅ': 'ziu',
+			'ぢぇ': 'zie',
+			'ぢょ': 'zio',
+		});
+	}
+
+	if (config['づ'] === 'zu') {
+		extend(table, {
+			'づ': 'zu',
+			'づぁ': 'zua',
+			'づぃ': 'zui',
+			'づぇ': 'zue',
+			'づぉ': 'zuo',
+		});
+	}
+
+	if (config['ゐ'] === 'i') {
+		extend(table, {
+			'ゐ': 'i',
+			'ゑ': 'e',
+		});
+	}
+
+	if (config['を'] === 'o') {
+		extend(table, {
+			'を': 'o',
+		});
 	}
 
 	return config;
